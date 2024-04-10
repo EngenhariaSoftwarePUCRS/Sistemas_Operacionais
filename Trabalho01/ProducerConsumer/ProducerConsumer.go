614050a47c82d50a403c84b6623279d218353649
Felipe Freitas <f.freitas007@edu.pucrs.br>
2024-04-10 00:15:15 -0300

    Trabalho 01 - Producer Consumer - Fixed Mutex
    And updated print logic
    
    Co-authored-by: Mateus Caçabuena <mateuscacabuena@gmail.com> Co-authored-by: Luiza Heller Kroeff Plá <LuHellerKP@users.noreply.github.com> Co-authored-by: Carolina Ferreira <carolmic@users.noreply.github.com>

--- Trabalho01/ProducerConsumer/ProducerConsumer.go
+++ Trabalho01/ProducerConsumer/ProducerConsumer.go
@@ -8,32 +8,20 @@ import (
 )
 
 const (
-	N          = 5 // Thread Count
+	PRODUCERS_COUNT = 2
+	CONSUMERS_COUNT = 6
+	THREAD_COUNT = PRODUCERS_COUNT + CONSUMERS_COUNT
 	QUEUE_SIZE = 50
 )
 
 var (
-	criticalSection  *Mutex = NewMutex()
-	items  *Mutex = NewMutex()
-	emptyQueue *Mutex = NewMutex()
-	queue  Queue  = make(Queue, 0, QUEUE_SIZE)
+	mutex *Mutex = NewMutex()
+	queue  Queue = make(Queue, 0, QUEUE_SIZE)
 )
 
-func Producer(id int) {
-	for {
-		fmt.Println("\033[32m", "Pronto para produzir...", "\033[0m")
-		item := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(99)
-		emptyQueue.Lock(id)
-		criticalSection.Lock(id)
-		queue.Enqueue(item)
-		fmt.Println("\033[32m",
-			id, "- Adicionando: ", item, "\n",
-			id, "- Fila Critical Section: ", queue, "\n",
-			id, "- TimeStamp: ", CurrentTimeStamp(), "\n",
-		"\033[0m")
-		criticalSection.UnlockOthers(id)
-		items.UnlockOthers(id)
-	}
+// Returns a random integer between 0 and max
+func GetRandom(max int) int {
+	return rand.Intn(max + 1)
 }
 
 // WORKAROUND
@@ -45,44 +33,72 @@ func CurrentTimeStamp() string {
 	return fmt.Sprint(milliseconds)
 }
 
-func Consumer(idProducer int, idConsumer int) {
+func Producer(id int) {
 	for {
-		items.Lock(idConsumer)
-		fmt.Println("\033[34m", idConsumer, "- Tentando consumir...", "\033[0m")
-		criticalSection.Lock(idConsumer)
-		previousQueue := queue
-		item, success := queue.Dequeue()
-		if success {
-			fmt.Println("\033[31m",
-				idConsumer, "- Anterior: ", previousQueue, "\n",
-				idConsumer, "- Removido: ", item, "\n",
-				idConsumer, "- New:", queue, "\n",
-				idConsumer, "- TimeStamp: ", CurrentTimeStamp(), "\n",
-			"\033[0m")
-		}
-		criticalSection.UnlockOthers(idConsumer)
-		emptyQueue.Unlock(idProducer)
+		mutex.Lock(id)
+			fmt.Println("\033[34m", id, "- Pronto para produzir...")
+			item := GetRandom(99)
+			success := queue.Enqueue(item)
+			if success {
+				fmt.Println("\tAdicionando: ", item)
+				fmt.Println("\tFila Critical Section: ", queue)
+				fmt.Println("\tTimeStamp: ", CurrentTimeStamp())
+			} else {
+				fmt.Println("\033[31m", "\tFila cheia, não foi possível adicionar")
+			}
+			fmt.Println("\033[0m")
+		mutex.Unlock(id)
+	}
+}
+
+func Consumer(id int) {
+	for {
+		mutex.Lock(id)
+			fmt.Println("\033[33m", id, "- Tentando consumir...")
+			previousQueue := queue
+			item, success := queue.Dequeue()
+			if success {
+				fmt.Println("\tAnterior: ", previousQueue)
+				fmt.Println("\tRemovido: ", item)
+				fmt.Println("\tNew:", queue)
+				fmt.Println("\tTimeStamp: ", CurrentTimeStamp())
+			} else {
+				fmt.Println("\033[31m", "\tFila vazia, não foi possível remover")
+			}
+			fmt.Println("\033[0m")
+		mutex.Unlock(id)
 	}
 }
 
 func main() {
-	runtime.GOMAXPROCS(N)
-	fmt.Printf("Producer Consumer with Queue Size %d\n", QUEUE_SIZE)
-	producerId := 0
-	go Producer(producerId)
-	for i := 1; i < N; i++ {
-		go Consumer(producerId, i)
+	runtime.GOMAXPROCS(THREAD_COUNT)
+	fmt.Println("\033[0m")
+	fmt.Println("\033[1m", "Producer Consumer with: ")
+	fmt.Println("\tQueue Size:\t", QUEUE_SIZE)
+	fmt.Println("\033[34m", "\t", "Producers: ", "\t", PRODUCERS_COUNT)
+	fmt.Println("\033[33m", "\t", "Consumers: ", "\t", CONSUMERS_COUNT)
+	fmt.Println("\033[0m")
+
+	for i := 0; i < PRODUCERS_COUNT; i++ {
+		go Producer(i)
 	}
-	<-time.After(300 * time.Millisecond)
+
+	for i := 0; i < CONSUMERS_COUNT; i++ {
+		go Consumer(i + PRODUCERS_COUNT)
+	}
+
+	// Stops the program after some delay, so the user can see the output
+	<- time.After(500 * time.Millisecond)
 }
 
 type Queue []int
 
-func (q *Queue) Enqueue(item int) {
+func (q *Queue) Enqueue(item int) (success bool) {
 	if len(*q) == QUEUE_SIZE {
-		return
+		return false
 	}
 	*q = append(*q, item)
+	return true
 }
 
 func (q *Queue) Dequeue() (head int, success bool) {
@@ -95,35 +111,13 @@ func (q *Queue) Dequeue() (head int, success bool) {
 }
 
 type Mutex struct {
-	// volatile bool entering[0..N] := 0
-	entering [N]bool
-	// volatile int number[0..N] := 0
-	number [N]int
+	number [THREAD_COUNT]int
 }
 
 func NewMutex() *Mutex {
 	return &Mutex{
-		entering: [N]bool{},
-		number:   [N]int{},
+		number: [THREAD_COUNT]int{},
 	}
-}
-
-func (m *Mutex) printEntering() {
-	fmt.Print("Entering: ")
-	fmt.Print("[")
-	for i := 0; i < len(m.entering); i++ {
-		fmt.Printf("%d ", m.entering[i])
-	}
-	fmt.Println("\b]")
-}
-
-func (m *Mutex) PrintNumber() {
-	fmt.Print("Number: ")
-	fmt.Print("[")
-	for i := 0; i < len(m.number); i++ {
-		fmt.Printf("%d ", m.number[i])
-	}
-	fmt.Println("\b]")
 }
 
 func (m *Mutex) maxNumber() int {
@@ -141,13 +135,8 @@ func (m *Mutex) maxNumber() int {
 }
 
 func (m *Mutex) Lock(i int) {
-	m.entering[i] = true
 	m.number[i] = 1 + m.maxNumber()
-	m.entering[i] = false
-	for j := 0; j < N; j++ {
-		for m.entering[j] {
-			// Wait until thread j receives its number:
-		}
+	for j := 0; j < THREAD_COUNT; j++ {
 		for m.number[j] != 0 && (m.number[j] < m.number[i] || (m.number[j] == m.number[i] && j < i)) {
 			// Wait until all threads with smaller numbers or with the same
 			// number, but with higher priority, finish their work
@@ -157,12 +146,4 @@ func (m *Mutex) Lock(i int) {
 
 func (m *Mutex) Unlock(i int) {
 	m.number[i] = 0
-}
-
-func (m *Mutex) UnlockOthers(i int) {
-	for j := 0; j < N; j++ {
-		if j != i {
-			m.number[j] = 0
-		}
-	}
 }
